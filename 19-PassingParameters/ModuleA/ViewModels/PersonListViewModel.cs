7ca6421275a1af07978857ac253af45ff973f336
TechKind <ken_hayakawa@tech-kind.biz>
2022-02-23 14:02:48 +0900

    update 2022.02.23

---
+++ 19-PassingParameters/ModuleA/ViewModels/PersonListViewModel.cs
@@ -0,0 +1,64 @@
+﻿using ModuleA.Business;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA.ViewModels
+{
+    public class PersonListViewModel : BindableBase
+    {
+        IRegionManager _regionManager;
+
+        private ObservableCollection<Person>? _people;
+
+        public ObservableCollection<Person>? People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
+
+        public DelegateCommand<Person> PersonSelectedCommand { get; private set; }
+
+        public PersonListViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            PersonSelectedCommand = new DelegateCommand<Person>(PersonSelected);
+            CreatePeople();
+        }
+
+        private void PersonSelected(Person person)
+        {
+            var parameters = new NavigationParameters();
+            parameters.Add("person", person);
+
+            if (person != null)
+            {
+                _regionManager.RequestNavigate("PersonDetailsRegion", "PersonDetail", parameters);
+            }
+        }
+
+        private void CreatePeople()
+        {
+            var people = new ObservableCollection<Person>();
+            for (int i = 0; i < 10; i++)
+            {
+                people.Add(new Person()
+                {
+                    FirstName = string.Format("First {0}", i),
+                    LastName = string.Format("Last {0}", i),
+                    Age = i
+                });
+            }
+
+            People = people;
+        }
+
+    }
+}
---
+++ 20-ConfirmCancelNavigation/ConfirmCancelNavigation/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Unity;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ConfirmCancelNavigation.Views;
+using Prism.Modularity;
+
+namespace ConfirmCancelNavigation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleA.ModuleAModule>();
+        }
+    }
+}
